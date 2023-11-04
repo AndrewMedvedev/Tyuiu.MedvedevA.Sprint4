0215167726dbd0c54302ced4c56ac6660e096522
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-11-04 16:30:48 +0500

    Task0

---
+++ Tyuiu.MedvedevA.Sprint4.Task0.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevA.Sprint4.Task0.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevA.Sprint4.Task0.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a2e3bbd9-9d3c-46a2-8307-d3b1e2507e79")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MedvedevA.Sprint4.Task0.V27/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MedvedevA.Sprint4.Task0.V27.Lib;
+
+namespace Tyuiu.MedvedevA.Sprint4.Task0.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("- ИСХОДНЫЕ ДАННЫЕ:                                                        -");
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            int[] numsArray = { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 };
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i <= numsArray.Length - 1; i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("- РЕЗУЛЬТАТ:                                                              -");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Произведение нечетных элементов массива = " + ds.GetMultOddArrEl(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
