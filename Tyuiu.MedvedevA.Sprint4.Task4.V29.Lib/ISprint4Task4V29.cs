cd446ffddbb53dfaed214a0ed2324d14a809ca6e
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-11-12 07:14:21 +0500

    Task4

---
+++ Tyuiu.MedvedevA.Sprint4.Task4.V29.Lib/ISprint4Task4V29.cs
@@ -0,0 +1,6 @@
+﻿namespace Tyuiu.MedvedevA.Sprint4.Task4.V29.Lib
+{
+    public interface ISprint4Task4V29
+    {
+    }
+}
---
+++ Tyuiu.MedvedevA.Sprint4.Task4.V29/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MedvedevA.Sprint4.Task4.V29.Lib;
+
+namespace Tyuiu.MedvedevA.Sprint4.Task4.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"Введите {i},{j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mtrx);
+            Console.WriteLine("Сумма четных элементов массива = " + res);
+            Console.ReadKey();
+        }
+    }
+}
